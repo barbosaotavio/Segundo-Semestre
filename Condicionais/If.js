86f0ac7269545f236ded56070826a6821ee0870f
barbosaotavio <barbosaotavio293@gmail.com>
2026-04-08 21:37:24 -0300

    if else

---
+++ Condicionais/If.js
@@ -0,0 +1,7 @@
+let a = eval (prompt('Numero'));
+    if(a % 2 == 0){
+        alert("O número é par");
+        console.log(`Valor:${a}`)
+
+    }
+    
---
+++ Condicionais/If_Else.js
@@ -0,0 +1,8 @@
+let a = eval (prompt('Numero'));
+    if(a % 2 == 0)
+        alert("O número é par");
+
+         else 
+            alert("O número é ímpar")
+
+    
